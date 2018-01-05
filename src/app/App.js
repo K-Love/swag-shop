cfd06ba1d71aff49102c5aa1c03ac2decfcabcab
K-Love <35044866+K-Love@users.noreply.github.com>
2018-01-05 09:38:47 -0800

    Day 4 Update, Created Product Component

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Product from '../product/product';
 import HttpService from '../services/http-service';
 
 const http = new HttpService();
@@ -26,11 +27,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          <h2 className="App-subhead">Future Home of K Love&apos;s Swag Shop</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-main">
+          <Product />
+        </div>
       </div>
     );
   }
