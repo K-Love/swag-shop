64ecbeae7418db2768c05e9a58b30692717aacff
K-Love <35044866+K-Love@users.noreply.github.com>
2018-01-08 12:28:09 -0800

    Day 6 Update
    Worked with state in React to get the shop's product data from the api.

--- src/product/product.js
+++ src/product/product.js
@@ -5,7 +5,7 @@ class Product extends Component {
     render() {
         return (
             <div className="card product">
-                <img className="card-img-top" src={this.props.imgURL} alt="Product" />
+                <img className="card-img-top" src={this.props.imgURL} alt="Product"></img>
                 <div className="card-block">
                     <h4 className="card-title">{this.props.title}</h4>
                     <p className="card-text">Price: ${this.props.price}</p>
