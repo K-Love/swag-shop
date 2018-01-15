8cbc7a390c0804438e071eb585d95e114752c443
K-Love <35044866+K-Love@users.noreply.github.com>
2018-01-15 10:03:25 -0800

    Day 13 of #100DaysOfCode
    Made the wish list listen for real product data

--- src/wishlist/wishlist.js
+++ src/wishlist/wishlist.js
@@ -1,35 +1,33 @@
 import React, {Component} from 'react';
 import './wishlist.css';
 import DataService from '../services/data-service';
-import NotificationService from '../services/notification-service';
+import NotificationService, {NOTIF_WISHLIST_CHANGED} from '../services/notification-service';
 import ProductCondensed from '../product-condensed/product-condensed';
+
+let ns = new NotificationService();
 
 class WishList extends Component {
 
     constructor(props) {
         super(props);
 
-        this.state={wishList:[
-            {
-                title:"Bologna Killer",
-                price:23.99,
-                _id:"234885"
-            },
-            {
-                title:"Foo Man Chu",
-                price:4.54,
-                _id:"2349877"
-            },
-            {
-                title:"Pipe Dream",
-                price:100,
-                _id:"264997"
-            }
-        ]
-    };
+        this.state={wishList:[]};
 
         //Bind Functions
         this.createWishList = this.createWishList.bind(this);
+        this.onWishListChanged = this.onWishListChanged.bind(this);
+    }
+
+    componentDidMount() {
+        ns.addObserver(NOTIF_WISHLIST_CHANGED, this, this.onWishListChanged);
+    }
+
+    componentWillUnmount() {
+        ns.removeObserver(this, NOTIF_WISHLIST_CHANGED);
+    }
+
+    onWishListChanged(newWishList) {
+        this.setState({wishList: newWishList});
     }
 
     createWishList = () => {
